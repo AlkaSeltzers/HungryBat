0d7eb489e46b8439bbdec0f9774223e47634bc0c
sb4091 <39011907+sb4091@users.noreply.github.com>
2018-05-10 14:57:48 -0400

    Update Bat.java

--- src/Bat.java
+++ src/Bat.java
@@ -2,28 +2,35 @@
  * 
  */
 
-/**
+/** A Bat is an Asset that maintains information about its position and knows how to bounce and fall
  * @author alka
  *
  */
 public class Bat extends Asset {
 
 	/**
-	 * 
+	 * Constructs a bat at (0,0) using initialize method
 	 */
 	public Bat() {
 
 		initialize();
 	}
-	
+	/*
+	 * Initializes a bat at Position (0,0)
+	 */
 	public void  initialize(){
 		Position batPos = new Position(0,0);
 		
 	}
-
+	/*
+	 * Makes the bat bounce upwards by increasing y-coordinate
+	 */
 	public void bounce(){
 		
 	}
+	/*
+	 * Makes the bat free fall by decreasing y-coordinate
+	 */
 	public void fall(){
 		
 	}
