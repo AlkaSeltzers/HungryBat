e3d9fc716d16b37e0f1f92930402226b72f3888c
sb4091 <39011907+sb4091@users.noreply.github.com>
2018-05-10 15:09:46 -0400

    Update Display.java

--- src/Display.java
+++ src/Display.java
@@ -4,32 +4,49 @@ import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 
 /**
- * 
+ * A Display is a JPanel that implements Runnable and that can initialize its panel and Assets, run its threads, paint components, animate itself, and respond to a keypress 
  */
 
 /**
- * @author alka
+ * @author Alka, Subhiksha, Rubaiya
  *
  */
 public class Display extends JPanel implements Runnable{
 
 	/**
-	 * 
+	 * Constructs a Display
 	 */
 	public Display() {
 	}
-	
+	/** 
+	 * Runs the threads
+	 */
 	public void run() {
 		
 	}
+	/** 
+	 * Initializes the panel
+	 */
 	public void initializePanel () {}
+	/**
+	 * Initializes the Assets
+	 */
 	public void initializeAssets() {
 		Bat bat = new Bat();
 		Prize prize = new Prize();
 		Obstacle obstacle = new Obstacle();
 	}
+	/** 
+	 * Paints the components on the panel
+	 */
 	public void paintComponent(Graphics g) {}
+	/**
+	 * Does the animation
+	 */
 	public void animation() {}
+	/**
+	 * Bounces the bat upon a keypress
+	 */
 	public void keyPressed(KeyEvent e) {
 		bat.bounce();
 	} 
