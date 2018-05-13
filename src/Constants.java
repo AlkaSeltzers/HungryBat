7dd32c19c8d620e33b6dc56ebab5dcbc148d3b28
AlkaSeltzers <32341871+AlkaSeltzers@users.noreply.github.com>
2018-05-12 23:06:34 -0400

    version 1.2

--- src/Constants.java
+++ src/Constants.java
@@ -8,4 +8,8 @@
  */
 public class Constants {
 	public static int MAX_PRIZES;
+	public static int MAX_OBSTACLES;
+	public static int FRAME_WIDTH = 640;
+	public static int FRAME_HEIGHT = 480;
+	public static String IMAGE_FOLDER = "./images/";
 }
--- src/Display.java
+++ src/Display.java
@@ -1,7 +1,14 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
+
+
 
 /**
  * A Display is a JPanel that implements Runnable and that can initialize its panel and Assets, run its threads, paint components, animate itself, and respond to a keypress 
@@ -12,16 +19,52 @@ import javax.swing.JPanel;
  *
  */
 public class Display extends JPanel implements Runnable{
+	Bat bat;
+	Prize prize;
+	Obstacle obstacle;
+	Thread thread;
+	 private Dimension dimension;
 
 	/**
 	 * Constructs a Display
 	 */
 	public Display() {
+		// TODO Auto-generated constructor stub
+		initializePanel ();
+		initializeAssets();
+		initializeGame();
+		
+		 
+	              
 	}
 	/** 
 	 * Runs the threads
 	 */
+	@Override
 	public void run() {
+		while(true) {
+			animation();		        
+			repaint();
+			try {
+				thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public void initializePanel () {
+		setFocusable(true);
+		dimension = new Dimension(Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT);
+        setBackground(Color.blue);
+     }
+	
+	
+	public void initializeGame() {
+		thread = new Thread(this);
+		thread.start();
 		
 	}
 	/** 
@@ -32,24 +75,43 @@ public class Display extends JPanel implements Runnable{
 	 * Initializes the Assets
 	 */
 	public void initializeAssets() {
-		Bat bat = new Bat();
-		Prize prize = new Prize();
-		Obstacle obstacle = new Obstacle();
+		bat = new Bat();	
+		prize = new Prize();
+		obstacle = new Obstacle();
+		
+	}
+	private void drawBackground(Graphics g) {
+	    g.setColor(Color.blue);
+        g.fillRect(0, 0, dimension.width, dimension.height);
+        g.setColor(Color.green);
 	}
 	/** 
 	 * Paints the components on the panel
 	 */
-	public void paintComponent(Graphics g) {}
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		drawBackground(g);
+		drawBat(g);
+		Toolkit.getDefaultToolkit().sync();
+        g.dispose();
+	}
 	/**
 	 * Does the animation
 	 */
-	public void animation() {}
+	public void animation() {
+		bat.fall();		
+	}
 	/**
 	 * Bounces the bat upon a keypress
 	 */
 	public void keyPressed(KeyEvent e) {
 		bat.bounce();
-	} 
+	}
+	
+	private void drawBat(Graphics g) {
+		g.drawImage(bat.getImage(),bat.getPosition().getxPos(),bat.getPosition().getyPos(),this);
+	}
 
 
 }
